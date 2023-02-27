58f52556c3a9fd905ad6d1331d1dd949355a4fed
George Sablukov <justcopper31@gmail.com>
2023-02-27 16:25:19 +0500

    Updated 2.0

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isVisible
 import com.example.myapplication.databinding.ActivityMainBinding
 
 
@@ -11,11 +9,6 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
 
-    class MyHandler {
-        fun onDelete() {
-            // ...
-        }
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -23,15 +16,8 @@ class MainActivity : AppCompatActivity() {
 
 
         binding.mybutton.setOnClickListener(){
+        binding.textView.text = binding.input.text
 
-            if (binding.mybutton.text == "Отобразить") {
-                binding.mybutton.text = "Скрыть"
-                binding.textView.isVisible = true
-            }
-            else {
-                binding.mybutton.text = "Отобразить"
-                binding.textView.isVisible = false
-            }
         }
 
     }
